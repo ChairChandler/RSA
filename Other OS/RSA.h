d6842bb9b8419f2ab55641b496943321717c3aa6
ChairChandler <32361814+ChairChandler@users.noreply.github.com>
2018-06-10 18:02:33 +0200

    v1.0

---
+++ Other OS/RSA.h
@@ -0,0 +1,37 @@
+#ifndef RSA_H
+#define RSA_H
+#define MAX_NUM 4
+#define EQUALS 0
+#define MAX_DIGITS 2048
+#define MAX_SIZE 200
+#define MIN_SIZE 3
+#include "BigDec.h"
+
+typedef struct RSA_Keys_ {
+	
+	struct {
+		
+		BigDec e,n;
+		
+	} Public;
+	
+	struct {
+		
+		BigDec d,n;
+		
+	} Private;
+	
+} RSA_Keys;
+
+
+RSA_Keys RSA_generateKeys(size_t length);
+BigDec* RSA_encrypt(char *number, BigDec e, BigDec n);
+char* RSA_decrypt(BigDec *number, BigDec d, BigDec n, size_t length);
+
+static void RSA_publicKey(BigDec *eAdress, BigDec *nAdress, BigDec *xAdress, size_t length);
+static void RSA_privateKey(BigDec *dAdress, BigDec x, BigDec e);
+static BigDec RSA_modInv(BigDec a, BigDec b);
+static BigDec RSA_checkPrime(BigDec number);
+static BigDec RSA_numGenerate(size_t length);
+static BigDec RSA_relPrimeNum(BigDec number);
+#endif
